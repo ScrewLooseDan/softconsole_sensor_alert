c2bc6c68696a4311fa0238f3280411a8a15e23f9
ScrewLooseDan <9346191+ScrewLooseDan@users.noreply.github.com>
2019-01-16 17:08:50 -0500

    Switching over to TSL2561 sensor

--- sensor.py
+++ sensor.py
@@ -15,12 +15,26 @@ class SetDim(object):
     @staticmethod 
     def SensorOut(self):
         var = "System:DimLevel"
-        bus = smbus.SMBus(1)
+        try:
+            bus = smbus.SMBus(1)
+        except:
+            logsupport.Logs.Log("Error when attempting to write to light sensor, is it attached?", severity=ConsoleWarning)
+            return
+        '''
+        # For VCN4010
         bus.write_byte_data(0x13, 0x80, 0x2F)
         bus.write_byte_data(0x13, 0x84, 0x9D)
-        time.sleep(0.8)
-        data = bus.read_i2c_block_data(0x13, 0x85, 2)
-        luminance = data[0] * 256 + data[1]
+        '''
+        # For TLS2561
+        # Turn on sensor
+        bus.write_byte_data(0x39, 0x00 | 0x80, 0x03)
+        # sets timing register, 402ms (which is default, not sure it's needed)
+        bus.write_byte_data(0x39, 0x01 | 0x80, 0x02)
+        # give sensor time for reading
+        time.sleep(0.5)
+        '''
+        data = bus.read_i2c_block_data(0x13, 0x85, 2) # For VCN4010
+        luminance = data[0] * 256 + data[1] 
         lumStr = str(luminance)
         if luminance < 4:
             valuestore.SetVal(var, 5)
@@ -31,5 +45,26 @@ class SetDim(object):
         else:
             valuestore.SetVal(var, 25)
             logsupport.Logs.Log("luminance: " + lumStr, severity=ConsoleDebug)
+        '''
+        # Retrieves data
+        data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
+        data1 = bus.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
+        # This calculates visible lux (subtracts IR only from total)
+        vislux = (data[1] * 256 + data[0]) - (data1[1] * 256 + data1[0])
+        visluxStr = str(vislux)
+        if vislux < 4:
+           valuestore.SetVal(var, 5)
+           logsupport.Logs.Log("lux under 4: " + visluxStr, severity=ConsoleDebug)
+        elif vislux < 10:
+            valuestore.SetVal(var, 10)
+            logsupport.Logs.Log("lux under 10: " + visluxStr, severity=ConsoleDebug)
+        else:
+            valuestore.SetVal(var, 10)
+            logsupport.Logs.Log("lux: " + visluxStr, severity=ConsoleDebug)
+        # turns off sensor
+        bus.write_byte_data(0x39, 0x00 | 0x80, 0x03)
+
+
+
 
 alerttasks.alertprocs["SetDim"] = SetDim
